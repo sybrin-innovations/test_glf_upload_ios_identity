e30ee7d81a5b0c2c80c8c9a833ae63f98490243f
sybrin-innovations <54102867+sybrin-innovations@users.noreply.github.com>
2022-05-16 11:28:33 +0200

    framework files

---
+++ Sybrin_iOS_Identity.framework/Headers/Sybrin_iOS_Identity.h
@@ -0,0 +1,19 @@
+//
+//  Sybrin_iOS_Identity.h
+//  Sybrin.iOS.Identity
+//
+//  Created by Nico Celliers on 2020/08/06.
+//  Copyright © 2020 Sybrin Systems. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Sybrin_iOS_Identity.
+FOUNDATION_EXPORT double Sybrin_iOS_IdentityVersionNumber;
+
+//! Project version string for Sybrin_iOS_Identity.
+FOUNDATION_EXPORT const unsigned char Sybrin_iOS_IdentityVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Sybrin_iOS_Identity/PublicHeader.h>
+
+
